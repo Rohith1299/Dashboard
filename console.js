a29f9b4eda589a8cdab87f1747f8f2309a3f6d28
Rohith1299 <88533082+Rohith1299@users.noreply.github.com>
2022-08-30 15:55:06 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,8 @@
+function Login(){
+var Username=document.getElementById("username").value
+var Password=document.getElementById("password").value
+console.log("Your username is " + Username + "\nYour password " + Password);
+
+}
+
+
